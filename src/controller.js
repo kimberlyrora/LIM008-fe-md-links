28ff0cbf5b3e65fe070110b533fe33b3c62432a6
kimberlyrojasra <kimberlyrojasra@gmail.com>
2019-02-26 15:40:04 -0500

    se creó funciones convertToAbsolute que convierte ruta relativa a absoluta y función isDirectory que verifica si es un directorio o no, asi también se creó sus respectivos tests que corren al 100%

--- src/controller.js
+++ src/controller.js
@@ -1,11 +1,30 @@
 const path = require('path');
+const fs = require('fs');
 
 /**
  * @param {ruta a verificar} route
  * @returns boolean: true si es absoluta
  *                   false si es relativa
  */
-export const isAbsolute = (route) =>{
+export const isAbsolute = (route) => {
     const routeAbsolute = path.isAbsolute(route);
     return routeAbsolute;
+}
+/**
+ * 
+ * @param {ruta relativa a convertir a absoluta} route 
+ * @returns string: ruta absoluta
+ */
+export const convertToAbsolute = (route) => {
+    const routeAbsolute = path.resolve(route);
+    return routeAbsolute;
+}
+/**
+ * @param {ruta a verificar si es directorio} route
+ * @returns boolean: true si es directorio
+ *                   false si no es directorio
+ */
+export const isDirectory = (route) => {
+    const routeDirectory = fs.statSync(route).isDirectory();
+    return routeDirectory;
 }
--- test/controller.spec.js
+++ test/controller.spec.js
@@ -1,4 +1,4 @@
-import { isAbsolute } from '../src/controller.js';
+import { isAbsolute, convertToAbsolute, isDirectory } from '../src/controller.js';
 describe('isAbsolute', () => {
     it('Debería ser una función', () => {
         expect(typeof isAbsolute).toBe('function');
@@ -8,5 +8,25 @@ describe('isAbsolute', () => {
     });
     it('Debería devolver flase si es una función relativa', () => {
         expect(isAbsolute('.\\recursión')).toBe(false);
+    });
+});
+describe('convertToAbsolute', () => {
+    it('Debería ser una función', () => {
+        expect(typeof convertToAbsolute).toBe('function');
+    });
+    it('Debería devolver una ruta absoluta a partir de una ruta relativa', () => {
+        // console.log(convertToAbsolute('.\\recursión'));
+        expect(convertToAbsolute('.\\recursión')).toBe('C:\\Users\\Laboratoria\\Desktop\\Markdown\\LIM008-fe-md-links\\recursión');
+    });
+});
+describe('isDirectory', () => {
+    it('Debería ser una función', () => {
+        expect(typeof isDirectory).toBe('function');
+    });
+    it('Debería devolver true si la ruta es un directorio', () => {
+        expect(isDirectory('C:\\Users\\Laboratoria\\Desktop\\Markdown\\LIM008-fe-md-links')).toBe(true);
+    });
+    it('Debería devolver false si la ruta no es un directorio', () => {
+        expect(isDirectory('C:\\Users\\Laboratoria\\Desktop\\Markdown\\LIM008-fe-md-links\\src\\controller.js')).toBe(false);
     });
 });
