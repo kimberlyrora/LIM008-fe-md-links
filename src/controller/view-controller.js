becfc367bc5cca3a3f02d65d2af918798d30ec90
kimberlyrojasra <kimberlyrojasra@gmail.com>
2019-03-18 00:51:22 -0500

    haciendo cambios señalados en entrevista tecnica

--- src/controller/view-controller.js
+++ src/controller/view-controller.js
@@ -1,18 +1,10 @@
-import {isAbsolute, convertToAbsolute} from './pathAndFs.js';
+import {convertToAbsolute} from './pathAndFs.js';
 /**
  * Función donde se arma condicionales para la lectura de ruta y aplicar primeras funciones 
  * @param {Ruta a la que se le aplicará las funciones isAbsolute y convertToAbsolute} ruta
  * @returns string: ruta absoluta 
  */
-export const readRoute = ruta => {
-  let rutaAbs;
-  if (isAbsolute(ruta)) {
-    rutaAbs = ruta;
-  } else {
-    rutaAbs = convertToAbsolute(ruta);
-  }
-  return rutaAbs;
-};
+export const readRoute = ruta => convertToAbsolute(ruta);
 /**
  * Función que crea templates a partir del array pasado como parametro
  * @param {Array de objetos} data
@@ -20,17 +12,8 @@ export const readRoute = ruta => {
  */
 export const crearTemplateDeArray = (data) => {
   let templateListOfLinks = '';
-  let obj;
   data.forEach((link) => {
-    if (link.status === undefined && link.statusText === undefined) {
-      obj = `
-        ${link.file}, ${link.href}, ${link.text}
-        `;
-    } else { 
-      obj = `
-        ${link.file}, ${link.href}, ${link.text}, ${link.status}, ${link.statusText}
-        `;
-    }
+    let obj = `${link.file}, ${link.href}, ${link.text}, ${link.status ? link.status : ''}, ${link.statusText ? link.statusText : '' }\n`;
     templateListOfLinks += obj;
   });
   return templateListOfLinks;
