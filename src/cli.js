ed108e31d4762a6d8953d56132823024946113cd
kimberlyrojasra <kimberlyrojasra@gmail.com>
2019-03-20 16:19:50 -0500

    se exporto función cliJs

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
 const [,, ...args] = process.argv;
+const cliJs = require('./controller/cli-controller.js');
 cliJs(args).then(resp => console.log(resp));
--- src/controller/cli-controller.js
+++ src/controller/cli-controller.js
@@ -1,7 +1,7 @@
 import { crearTemplateDeArray } from './view-controller.js';
 import { stats } from './stats.js';
 const mdLinks = require('./mdLinks');
-export const cliJs = (args) => new Promise((resolve) => {
+const cliJs = (args) => new Promise((resolve) => {
   if (args[1] === '--validate' && args[2] === '--stats') {
     mdLinks(args[0], { validate: true }).then(resp => resolve(stats(resp, 'validate')));
   } else if (args[1] === '--validate') {
@@ -14,3 +14,4 @@ export const cliJs = (args) => new Promise((resolve) => {
     mdLinks(args[0], { validate: false }).then(resp => resolve(crearTemplateDeArray(resp)));
   }
 });
+module.exports = cliJs;
